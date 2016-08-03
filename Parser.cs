2cd485d091dcf4009cd9f0be5ccaca3a2f273e06
Dan Behlings <Dan Behlings>
2016-08-02 19:48:45 -0700

    little little bit more

--- Parser.cs
+++ Parser.cs
@@ -4,7 +4,26 @@ namespace DiceStream {
 
     public class Parser : IParser {
         public IParsed Parse(string rawStatement) {
-            
+            // now we tke the raw string and parse it into a statement
+				// how fancy do we want to get about it?
+				// if we write a grammar for it and 
+				// make a state machine parser
+				
+				// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 
