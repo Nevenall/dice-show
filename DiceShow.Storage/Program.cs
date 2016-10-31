69a61f825899f784f4367c0ddd2ccaeffea3ce8c
nevenall <nevenall@hotmail.com>
2016-10-31 14:07:07 -0700

    setup environment variable config for Storage

--- DiceShow.Storage/Program.cs
+++ DiceShow.Storage/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
-namespace ConsoleApplication
+namespace DiceShow.Storage
 {
     public class Program
     {
+
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var config = new ConfigurationBuilder()
+					 .AddEnvironmentVariables("diceshow_")
+					 .Build();
+
+                System.Console.WriteLine(JsonConvert.SerializeObject(config.AsEnumerable(), Formatting.Indented));
+
+
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+            finally
+            {
+                System.Console.WriteLine("Press any key");
+                Console.ReadKey();
+            }
         }
     }
 }
