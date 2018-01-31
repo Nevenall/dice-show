32e8f02c9939274c267a92d91567d59357a2c072
Dan Behlings <nevenall@hotmail.com>
2018-01-31 13:57:46 -0800

    formatting

--- DiceShow.App/Startup.cs
+++ DiceShow.App/Startup.cs
@@ -12,76 +12,63 @@ using DiceShow.Storage;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
 
-namespace DiceShow.App
-{
+namespace DiceShow.App {
 
-	public class Startup
-	{
+    public class Startup {
 
-		public IConfigurationRoot Configuration { get; set; }
-		public IHostingEnvironment Environment { get; set; }
+        public IConfigurationRoot Configuration { get; set; }
+        public IHostingEnvironment Environment { get; set; }
 
 
-		public Startup(IHostingEnvironment env, ILoggerFactory factory)
-		{
-			Environment = env;
-			Configuration = new ConfigurationBuilder()
-							.AddEnvironmentVariables("diceshow_")
-							.Build();
-		}
+        public Startup(IHostingEnvironment env, ILoggerFactory factory) {
+            Environment = env;
+            Configuration = new ConfigurationBuilder()
+                            .AddEnvironmentVariables("diceshow_")
+                            .Build();
+        }
 
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logFactory)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-				// in dev mode serve all files including .scss
-				app.UseStaticFiles(new StaticFileOptions { ServeUnknownFileTypes = true });
-			}
-			else
-			{
-				app.UseExceptionHandler("/Error");
-				app.UseStaticFiles();
-			}
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logFactory) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+                // in dev mode serve all files including .scss
+                app.UseStaticFiles(new StaticFileOptions { ServeUnknownFileTypes = true });
+            } else {
+                app.UseExceptionHandler("/Error");
+                app.UseStaticFiles();
+            }
 
-			app.UseWebSockets();
-			app.UseSignalR();
-			app.UseMvc();
-		}
+            app.UseWebSockets();
+            app.UseSignalR();
+            app.UseMvc();
+        }
 
-		public IServiceProvider ConfigureServices(IServiceCollection services)
-		{
-			services.AddSignalR(options =>
-			{
-				if (Environment.IsDevelopment())
-				{
-					options.Hubs.EnableDetailedErrors = true;
-				}
-			});
+        public IServiceProvider ConfigureServices(IServiceCollection services) {
+            services.AddSignalR(options => {
+                if (Environment.IsDevelopment()) {
+                    options.Hubs.EnableDetailedErrors = true;
+                }
+            });
 
-			services.AddMvc();
+            services.AddMvc();
 
-			var cb = new ContainerBuilder();
+            var cb = new ContainerBuilder();
 
-			cb.RegisterType<Executer>().As<IEvaluator>();
-			cb.RegisterType<RandomRoller>().As<IRoller>();
-			cb.RegisterType<Parser>().As<IParser>();
+            cb.RegisterType<Executer>().As<IEvaluator>();
+            cb.RegisterType<RandomRoller>().As<IRoller>();
+            cb.RegisterType<Parser>().As<IParser>();
 
-			if (Environment.IsDevelopment())
-			{
-				cb.Register<InMemoryRepository>(c => new InMemoryRepository("InMemoryRepository")).As<IRepository>();
-			}
-			else
-			{
-				// cb.Register<DocumentDbRepository>(c=> new DocumentDbRepository("connectionString","databaseName")).As<IRepository>();
-				var connectionString = Configuration.GetValue<string>("storage_connection");
-				cb.Register<InMemoryRepository>(c => new InMemoryRepository("InMemoryRepository")).As<IRepository>();
-			}
+            if (Environment.IsDevelopment()) {
+                cb.Register<InMemoryRepository>(c => new InMemoryRepository("InMemoryRepository")).As<IRepository>();
+            } else {
+                // cb.Register<DocumentDbRepository>(c=> new DocumentDbRepository("connectionString","databaseName")).As<IRepository>();
+                var connectionString = Configuration.GetValue<string>("storage_connection");
+                cb.Register<InMemoryRepository>(c => new InMemoryRepository("InMemoryRepository")).As<IRepository>();
+            }
 
-			cb.Populate(services);
-			var container = cb.Build();
-			return container.Resolve<IServiceProvider>();
-		}
-	}
+            cb.Populate(services);
+            var container = cb.Build();
+            return container.Resolve<IServiceProvider>();
+        }
+    }
 
 }
